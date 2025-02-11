289a2744d71c8f1558de847e9e8d031d211d2188
Ithnoobs <thisisrith@gmail.com>
2025-02-11 18:57:28 +0700

    Add asset directory to pubspec.yaml and implement RowColumn screen with images

---
+++ lib/screens/row_column.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class RowColumn extends StatefulWidget {
+  const RowColumn({super.key});
+
+  @override
+  State<RowColumn> createState() => _RowColumnState();
+}
+
+class _RowColumnState extends State<RowColumn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Expanded(child: Image.asset("assets/crazybitch.png")),
+          Expanded(child: Image.asset("assets/pic1.png")),
+          Expanded(child: Image.asset("assets/pic2.png")),
+        ],
+      )
+    )
+  }
+}
